e026464b3fca86e6f0b7f983b59406caaa67b21f
gautam-21103052 <97442969+gautam-21103052@users.noreply.github.com>
2022-02-25 23:05:22 +0530

    Add files via upload

---
+++ assignment4/assignmen4_output.py
@@ -0,0 +1,72 @@
+ASSIGNMENT 4
+
+Question #1
+
+Move Disk 1 from P to Q
+Move Disk 2 from P to R
+Move Disk 1 from Q to R
+Move Disk 3 from P to Q
+Move Disk 1 from R to P
+Move Disk 2 from R to Q
+Move Disk 1 from P to Q
+
+Question #2
+ 
+Enter the number of rows in Pascal's Triangle: 6
+
+Using recursions:
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Using Iterative method:
+
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Question #3
+ 
+Enter the first integer: 4
+Enter the first integer: 3
+Remainder is:  1
+Quotient is:  1
+Both values are non zero
+{5}
+{5}
+{5, 6}
+{5, 6}
+{5, 6, 7}
+{5, 6, 7}
+{5, 6, 7}
+Immutable set:  frozenset({5, 6, 7})
+Largest value in the set:  7
+Hash value:  7
+
+Question #4
+ 
+Object created
+The name of the student it Gautam and SID is 21105052.
+Object destroyed
+
+QUESTION #5
+ 
+a. The updated salary of the employee Mehak is 70000
+b. Employee Viren record deleted
+ 
+QUESTION #6
+ 
+Enter the first word: arc
+
+Enter a new meaningful word to test your friendship: car
+
+Does the word makes sense?(y or n)
+y
+The friends pass the friendship test!!
